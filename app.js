98494bf2fc314a70cb1de46067b110a920beb48b
rikut0904 <rkt9tuba4robot@gmail.com>
2025-12-16 15:50:58 +0900

    fix/secret値の修正

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(
   session({
-    secret: process.env.SESSION_SECRET || 'change_this_secret',
+    secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
     cookie: {
